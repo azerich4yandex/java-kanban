51ff68a2ae3b898d123db322cddfb8181e11b522
Rustam Aliev <azerich@yandex.ru>
2025-02-17 21:31:04 +0300

    Test class for Epic entity implementation

---
+++ test/ru/yandex/practicum/scheduler/models/EpicTest.java
@@ -0,0 +1,151 @@
+package ru.yandex.practicum.scheduler.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.scheduler.managers.Managers;
+import ru.yandex.practicum.scheduler.managers.TaskManager;
+import ru.yandex.practicum.scheduler.models.enums.StatusTypes;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class EpicTest {
+
+    private static TaskManager taskManager;
+    private Epic epic1;
+    private Epic epic2;
+    private Subtask subtask1;
+    private Subtask subtask2;
+    private Subtask subtask3;
+
+
+    @BeforeAll
+    static void init() {
+        taskManager = Managers.getDefault();
+    }
+
+    @BeforeEach
+    void createEpicsAndTasks() {
+        epic1 = new Epic("First epic", "First epic description");
+        epic2 = new Epic("Second epic", "Second epic description");
+        taskManager.addNewEpic(epic1);
+        taskManager.addNewEpic(epic2);
+
+        subtask1 = new Subtask("First subtask", "First subtask description", epic1);
+        subtask2 = new Subtask("Second subtask", "Second subtask description", epic1);
+        subtask3 = new Subtask("Third subtask", "Third subtask description", epic1);
+        taskManager.addNewSubtask(subtask1);
+        taskManager.addNewSubtask(subtask2);
+        taskManager.addNewSubtask(subtask3);
+    }
+
+    @AfterEach
+    void clearEpicsAndSubtasks() {
+        taskManager.deleteEpics();
+    }
+
+    @DisplayName("Статус пустого эпика должен быть NEW")
+    @Test
+    void shouldNewStatusWithOutSubtasks() {
+        StatusTypes expected = StatusTypes.NEW;
+
+        assertEquals(expected, taskManager.getEpic(epic2.getId()).getStatus(), "Неправильный статус Эпика");
+    }
+
+    @DisplayName("Статус непустого эпика должен быть NEW, если все его задачи NEW")
+    @Test
+    void shouldNewStatusWithNewSubtasks() {
+        StatusTypes expected = StatusTypes.NEW;
+
+        assertEquals(expected, taskManager.getEpic(epic1.getId()).getStatus(), "Неправильный статус Эпика");
+    }
+
+    @DisplayName("Статус непустого эпика должен быть IN_PROGRESS, если статус хотя бы одной его задачи не NEW")
+    @Test
+    void shouldInProgressStatusWithAtLeastOneNotNewSubtask() {
+        StatusTypes expected = StatusTypes.IN_PROGRESS;
+
+        subtask1.setStatus(StatusTypes.DONE);
+        taskManager.updateSubtask(subtask1);
+        epic1.calculateStatus();
+
+        assertEquals(expected, taskManager.getEpic(epic1.getId()).getStatus(), "Неправильный статус Эпика");
+    }
+
+    @DisplayName("Статус непустого эпика должен быть DONE, если статус всех его задач DONE")
+    @Test
+    void shouldDoneStatusWithAllDoneSubtask() {
+        StatusTypes expected = StatusTypes.DONE;
+
+        subtask1.setStatus(StatusTypes.DONE);
+        taskManager.updateSubtask(subtask1);
+        subtask2.setStatus(StatusTypes.DONE);
+        taskManager.updateSubtask(subtask2);
+        subtask3.setStatus(StatusTypes.DONE);
+        taskManager.updateSubtask(subtask3);
+
+        assertEquals(expected, taskManager.getEpic(epic1.getId()).getStatus(), "Неправильный статус Эпика");
+    }
+
+
+    @DisplayName("Созданный и сохранённый списки подзадач эпика должны совпадать")
+    @Test
+    void getSubtasks() {
+        List<Subtask> expected = new ArrayList<>();
+
+        expected.add(subtask1);
+        expected.add(subtask2);
+        expected.add(subtask3);
+
+        assertEquals(expected, taskManager.getEpic(epic1.getId()).getSubtasks(), "Списки подзадач не совпадают");
+    }
+
+    @DisplayName("Созданная и сохранённая подзадачи в эпике должны совпадать")
+    @Test
+    void addNewSubtask() {
+        Subtask subtask = new Subtask("New subtask", "New subtask description", epic2);
+        epic2.addNewSubtask(subtask);
+        taskManager.addNewSubtask(subtask);
+
+        assertEquals(subtask, taskManager.getEpic(epic2.getId()).getSubtasks().getFirst(),
+                "Ожидаемая и добавленная подзадачи на совпадают");
+    }
+
+    @DisplayName("Описание подзадачи эпика должно сохраняться после сохранения эпика")
+    @Test
+    void updateSubtask() {
+        String expected = "Modified subtask description";
+        subtask2.setDescription(expected);
+        epic2.updateSubtask(subtask2);
+        taskManager.updateEpic(epic2);
+
+        assertEquals(expected, taskManager.getSubtask(subtask2.getId()).getDescription(), "Описание не изменилось");
+    }
+
+    @DisplayName("После удаление подзадачи количество подзадач в Эпике должно измениться")
+    @Test
+    void deleteSubtask() {
+        List<Subtask> expected = new ArrayList<>();
+        expected.add(subtask2);
+        expected.add(subtask3);
+
+        epic1.deleteSubtask(subtask1);
+        taskManager.updateEpic(epic2);
+
+        assertEquals(expected, taskManager.getEpic(epic1.getId()).getSubtasks(), "Списки не совпадают");
+    }
+
+    @DisplayName("Количество задач эпика не должно измениться после удаление некорректной подзадачи")
+    @Test
+    void shouldBeEmptyAfterDeletingWrongSubtask() {
+        List<Subtask> expected = new ArrayList<>();
+
+        epic2.deleteSubtask(subtask1);
+
+        assertEquals(expected, taskManager.getEpic(epic2.getId()).getSubtasks(), "Списки не совпадают");
+    }
+}
