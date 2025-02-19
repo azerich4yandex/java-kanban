bfe48c0b93ff9408086e01c839e997a46b5d0530
Rustam Aliev <azerich@yandex.ru>
2025-02-19 12:40:39 +0300

    Implementing the requirements of a reviewer. Fixing problems #3

--- test/ru/yandex/practicum/scheduler/models/EpicTest.java
+++ test/ru/yandex/practicum/scheduler/models/EpicTest.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.scheduler.models;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
