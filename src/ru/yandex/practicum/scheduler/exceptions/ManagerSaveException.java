c162e3bc16d746f813ff3fca29d721b3541e41bf
Rustam Aliev <azerich@yandex.ru>
2025-03-24 15:01:53 +0300

    Code-style fixing #2

--- src/ru/yandex/practicum/scheduler/exceptions/ManagerSaveException.java
+++ src/ru/yandex/practicum/scheduler/exceptions/ManagerSaveException.java
@@ -1,10 +1,11 @@
 package ru.yandex.practicum.scheduler.exceptions;
 
-public class ManagerSaveException extends Exception{
+public class ManagerSaveException extends Exception {
 
     public ManagerSaveException() {
         super();
     }
+
     public ManagerSaveException(Throwable cause) {
         super(cause);
     }
